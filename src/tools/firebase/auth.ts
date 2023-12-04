045cb3c946daa4a54312c49a3deaadf9613ae088
Octavio Martinez <omartinez1618@gmail.com>
2023-12-04 18:37:15 -0400

    register action

--- src/tools/firebase/auth.ts
+++ src/tools/firebase/auth.ts
@@ -1,15 +1,13 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./config";
 
-export function login(email:string, password:string) {
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log('user', user)
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log( errorCode, errorMessage)
-        });
+export async function login(email:string, password:string) {
+    return await signInWithEmailAndPassword(auth, email, password)
+}
+
+// operationType,
+// providerId,
+// user
+export async function register(email:string, password:string) {
+    return await createUserWithEmailAndPassword(auth, email, password)
 }
