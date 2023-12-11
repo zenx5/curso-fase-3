48b090bead3fa5aa842a22fd3c12337381b8dcb9
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2023-12-11 19:30:22 -0400

    fix bug

--- src/app/consulta/page.tsx
+++ src/app/consulta/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 // importa desde ../../tool/services/QueryService.ts
 import QueryService from "../../tools/services/QueryService";
 import CourseModel from "@/tools/models/CourseModel";
+import UserModel from "@/tools/models/UserModel";
 
 export default function Page() {
 
@@ -13,13 +14,20 @@ export default function Page() {
 
 
     const search = async ( email: string )=> {
-        const user: any = (await QueryService.query( email ))
-        console.log(user)
+        const query: any = (await QueryService.query( email ))
+        console.log(query)
 
-        const course = (await CourseModel.get( user.courseId ));
+        const course = (await CourseModel.get( query.courseId ));
         console.log(course)
 
-        setDatas({ user, course });
+        const users:any = await UserModel.get( query.userId );
+        console.log(users)
+
+        const user = users.find( ( user:any ) => {
+            return user.email == email
+        })
+
+        setDatas({ user, course, query });
     }
 
     const handlerFormSubmit = ( event: React.FormEvent<HTMLFormElement> ) => {
@@ -50,7 +58,7 @@ console.log(datas)
         </form>
         
         <div className="flex flex-col justify-center items-center">
-            { datas && datas.user?.length && datas.course?.length && (
+            { datas && datas.user && datas.query.length && datas.course?.length && (
 
             <div
                 className="w-full sm:max-w-2xl bg-yellow-100 flex flex-col items-center sm:items-start sm:rounded-lg mb-10 p-6"
@@ -61,10 +69,11 @@ console.log(datas)
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-full ">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
+                        {/* <image href={datas.user.image} /> */}
                     </div>
                     {/* Datos de usuarios */}
                     <div className="mb-4 text-center font-mono m-auto">
-                        <div className="font-bold">Nombre: Stephany Plaza</div>
+                        <div className="font-bold">Nombre: {datas.user.name}</div>
                         <div>Edad: 27</div>
                         <div>Cursos Realizados:</div>
                     </div>
@@ -73,23 +82,26 @@ console.log(datas)
                 <div className="mt-5 w-full flex flex-col items-center">
                     
                     {/* tecnologias vistas */}
-                    <div className="grid sm:grid-cols-3 flex-col justify-center items-center gap-2 text-center m-2 w-full max-w-xs sm:max-w-none sm:flex-row w-full flex-wrap">
+                    <div className="flex sm:flex-row flex-col justify-center items-center gap-2 text-center m-2 w-full max-w-xs sm:max-w-none sm:flex-row w-full flex-wrap">
+                        {
+                            datas.course.map( ( course: any ) => (
+                                <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
+                                    >{course.name}</a>
+                            ))
+                        }
                         {/* <div className="flex flex-col items-center sm:flex-row w-full"> */}
-                            <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
+                            {/* <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
                                 >Maquetacion Web</a>
                             <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
                                 >JavaScript</a>
                             <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
                                 >React</a>
-                        {/* </div>
-                        <div className="flex flex-col items-center sm:flex-row w-full"> */}
                             <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
                                 >NextJs</a>
                             <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full " href="#"
                                 >Tailwind</a>
                             <a className="font-sans font-bold rounded-lg p-2 m-2 content-around bg-blue-300 w-full" href="#"
-                                >Python</a>
-                        {/* </div> */}
+                                >Python</a> */}
                     </div>
                 </div>
 
